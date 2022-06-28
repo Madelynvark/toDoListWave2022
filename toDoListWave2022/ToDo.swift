c78b485b8615e49eed724703ab8e1ea95fa1115e
Mdelyn Varkey <Madelynvarkey@gmail.com>
2022-06-28 10:36:13 -0500

    Initial Commit

---
+++ toDoListWave2022/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  toDoListWave2022
+//
+//  Created by Saji Varkey on 6/27/22.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
---
+++ toDoListWave2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  toDoListWave2022
+//
+//  Created by Saji Varkey on 6/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
